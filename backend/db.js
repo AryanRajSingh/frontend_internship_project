3b12d1752f945e4aef50049282170d7157bf1b55
Aryan Raj <aryanraj844101@gmail.com>
2025-10-04 16:52:26 +0530

    Railway db connect

--- backend/db.js
+++ backend/db.js
@@ -7,6 +7,7 @@ async function getDBConnection() {
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
+      port: process.env.DB_PORT, 
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     });
--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ async function getDBConnection() {
   return await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT, 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   });
